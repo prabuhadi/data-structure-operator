8c9f1b88186149e4a5d77380f551e26775b8d7cb
prabuhadi <prabuhadiyo@gmail.com>
2023-02-27 15:48:59 +0700

    Destructuring and spread operator

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let currentDate = new Date();
+let currentHour = currentDate.getHours();
+let currentMinute = currentDate.getMinutes();
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -27,11 +31,72 @@ const restaurant = {
     },
   },
 
-  order: function (starterIndex, menuIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[menuIndex]];
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 'No Menu',
+    mainIndex = 'Empty',
+    time = `${currentHour}:${currentMinute}`,
+    address = 'Unknown Address',
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
   },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  starterIndex: 0,
+  mainIndex: 0,
+  address: 'La palma , 63',
+});
+
+// Destructuring Object //
+const { name, categories, openingHours } = restaurant; // if object use curly bracket
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values //
+const { mainMenu: menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
+
+// Mutating variable //
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 30, c: 34 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested object //
+const {
+  thu: { open: o, close: c },
+  fri,
+  sat,
+} = openingHours;
+console.log(o, c);
+
+// Destructuring Array //
 /*
 const arr = [22, 33, 23];
 const a = arr[0];
@@ -68,3 +133,49 @@ console.log(one, four);
 const [i = 2, j = 2, k = 2] = [8];
 console.log(i, j, k);
 */
+
+// Spread Operator //
+const arr = [7, 8, 9];
+const badNewArr = [2, arr[0], arr[2], arr[1], 10];
+console.log(badNewArr);
+
+const newArr = [2, 3, ...arr];
+console.log(newArr);
+
+console.log(...newArr); // ... for ouput without array properties
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+const mainMenuCopy = [...restaurant.mainMenu];
+const starterMenuCopy = [...restaurant.starterMenu];
+
+// Join 2 arrays //
+const menus = [...mainMenuCopy, ...starterMenuCopy];
+console.log(...menus);
+
+// Iterables : arrays, string, maps, stes. Not objects
+const str = 'Prabu';
+console.log(...str);
+const letters = [...str, '', 'H.'];
+console.log(letters);
+// console.log(`${str} Hadi`);
+
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1?"),
+  // prompt('Ingredient 2?'),
+  // prompt('Ingredient 3?'),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+// Object //
+const newRestaurant = { foundedIn: 1999, ...restaurant, founder: 'Prabu' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Restorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
